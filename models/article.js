cc63bf5fa8dcbf9e2cff01f9fa5adf93aa037a5d
3ddy925 <3ddy70@gmail.com>
2021-02-23 16:25:05 -0800

    message

---
+++ models/article.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class article extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  article.init({
+    title: DataTypes.STRING,
+    source: DataTypes.STRING,
+    date: DataTypes.STRING,
+    articleLink: DataTypes.STRING,
+    imageLink: DataTypes.STRING,
+    author: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'article',
+  });
+  return article;
+};
